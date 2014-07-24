21b307082728451578b7e130a011b18caf7dfde5
christinedt <c.j.decarteret@gmail.com>
2014-07-24 11:41:49 -0600

    Removed the test LED, added comments

--- twitter.py
+++ twitter.py
@@ -3,47 +3,42 @@ import time
 import RPi.GPIO as GPIO
 import camera
 
+#Variables to store your twitter authentication info
 APP_KEY = 'y41mgd4xnJny8S8YKdzhc5j1S'
 APP_SECRET = 'o1DSRJGGtZkBZANwu92yZMlBPiR31QEsAkRqPkefcRi1zS1N3y'
 OAUTH_TOKEN = '2505082970-OslkWNt2YHfVmiRFIuI4W0GgGFQB1n4cSsjYL9t'
 OAUTH_TOKEN_SECRET = 'G0eAgs48U7dlIeC0rehWlb0A6njw0QByquqnFbEe9gAKg'
 
-LED = 24
+#Pin connected to your button
 BUTTON = 23
-light_on = False
+
 x = 0
 
+#Set the pinmode for the board
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(LED, GPIO.OUT)
+#Set the mode for your button pin
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 def printFunction(channel):
 	global light_on
 	print("Button pressed")
 
+	#Run the function to take a picture
 	camera.cameramain()
 
+	#Pass authentication info into Twython
 	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 	
+	#Store the photo
 	photo = open('photo.jpg', 'rb')
+
+	#Tweet the photo
 	twitter.update_status_with_media(status='Checkout this cool image!', media=photo)
 
-	if(not light_on):
-		GPIO.output(LED, True)
-		light_on = True
-	else:
-		GPIO.output(LED, False)
-		light_on = False
-
+#Add event listener to listen for the button press
 GPIO.add_event_detect(BUTTON, GPIO.RISING, callback=printFunction, bouncetime=300)
 
 while True:
-	if(light_on):
-		x += 1
-	else:
-		x -= 1	
-
-#twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-#twitter.update_status(status='TWEET!')
+	x += 1
+	x -= 1	
